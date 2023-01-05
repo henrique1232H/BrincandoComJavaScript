dc138a7c3950f6f83d9f11b4f03c2c35beec4f06
ICEBERG776 <henriqueau111@gmail.com>
2023-01-04 23:11:57 -0300

    modified:   3-teste/assets/js/script.js 	modified:   3-teste/index.html

--- 3-teste/assets/js/script.js
+++ 3-teste/assets/js/script.js
@@ -1,16 +1,22 @@
+
 function enviar() {
     let inputButton = document.body.querySelector("#text").value;
     const show = document.body.querySelector(".mostrar");
     
+    
 
     document.body.style.fontSize = "15px"
     show.innerHTML += `<li>${inputButton}</li>`
+    document.body.style.background = "#ccc"
 
-    
+     return console.log("Tudo funcionando👍")
+
+   
 
 }
 
-function resetar() {
-    const show = document.body.querySelector(".mostrar");
-    show.innerHTML = "";
-}
+    function resetar() {
+        const show = document.body.querySelector(".mostrar");
+        show.innerHTML = "";
+        console.log("Resetado")
+    }
