0c7b97525f9684e05548c18cae5212afd96ebb91
ICEBERG776 <henriqueau111@gmail.com>
2023-01-05 22:48:37 -0300

    modified:   3-teste/assets/css/style.css 	modified:   3-teste/assets/js/script.js 	modified:   3-teste/index.html

--- 3-teste/assets/js/script.js
+++ 3-teste/assets/js/script.js
@@ -1,22 +1,27 @@
 
 function enviar() {
-    let inputButton = document.body.querySelector("#text").value;
+    let input = document.body.querySelector("#text").value;
     const show = document.body.querySelector(".mostrar");
-    
-    
+    document.body.style.fontSize = "15px";
+    show.innerHTML += `<li>${input.toUpperCase()}</li>`;
+    document.body.style.background = "#ccc";
 
-    document.body.style.fontSize = "15px"
-    show.innerHTML += `<li>${inputButton}</li>`
-    document.body.style.background = "#ccc"
-
-     return console.log("Tudo funcionando👍")
-
-   
+     return input;
 
 }
 
-    function resetar() {
-        const show = document.body.querySelector(".mostrar");
-        show.innerHTML = "";
-        console.log("Resetado")
-    }
+
+const teste = () => {
+    const pegar = enviar();
+    const array = [pegar, pegar];
+    document.body.style.background = "purple";
+    console.log(array);
+    return true;
+}
+
+const reset = () => {
+    const show = document.body.querySelector(".mostrar");
+    document.body.style.background = "#fff";
+    show.innerHTML = "";
+    console.log("Resetado");
+}
